dabeb95815d38de33e4b0a525bd673b7180cec2c
Ethan <afrikapetersek@gmail.com>
2024-06-10 00:24:55 +0200

    Object Types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-// Boolean Types mini-challenge
-// if the last reviewer is a loyalty User, can you add a star to the end of their name?
-// please do so in the existing function, and make sure to declare what type of 
-// parameters the function takes.
-// : boolean
+// Object Types
+// In this scrim, based on what we just learnt, can you change userName to firstName, then
+// add an age and lastName to the 'you' Object, making sure to account for it in the Specialised
+// Object for defining its structure.
 
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
+let isOpen: boolean
 
 const reviews = [
     {
@@ -27,11 +29,34 @@ const reviews = [
     },
 ]
 
-// Solution
-function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean) {
+
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
     const iconDisplay = isLoyalty ? '⭐' : ''
     reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
+
+const you: {
+    firstName : string;
+    lastName: string;
+    isReturning: boolean;
+    age: number;
+} = {
+   firstName: 'Bobby',
+   lastName: 'Brown',
+   isReturning: true,
+   age: 35
+}
+
+console.log(you.userName)
+
+function populateUser(isReturning : boolean, userName: string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
