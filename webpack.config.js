a792a6d0a907b6d4c27cc61fbf90ccd3d5199c2a
Ethan <afrikapetersek@gmail.com>
2024-06-10 00:13:37 +0200

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    index: './index.ts'
-  },
-  output: {
-    filename: '[name].pack.js',
-    path: path.resolve(__dirname, 'dist') // Ensure the output directory is 'dist'
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.json']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  }
+	mode: 'development', // or 'production'
+	entry: {
+		index: './src/index.ts',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'), // Ensure output path is defined
+		filename: '[name].pack.js',
+	},
+	resolve: {
+		extensions: ['.ts', '.js', '.json'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
 };
