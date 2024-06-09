5864bcf108eaeb54f0794f9f294e530a4c673ef7
Ethan <afrikapetersek@gmail.com>
2024-06-10 01:38:00 +0200

    commit

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,12 +1,23 @@
-enum LoyaltyUser {
-	GOLD_USER = 'GOLD_USER',
-	SILVER_USER = 'SILVER_USER',
-	BRONZE_USER = 'BRONZE_USER',
-}
+import { LoyaltyUser } from './enums';
+import { Price, Country } from './types';
 
 export interface Review {
 	name: string;
 	stars: number;
 	loyaltyUser: LoyaltyUser;
 	date: string;
+}
+
+export interface Property {
+	image: string;
+	title: string;
+	price: Price;
+	location: {
+		firstLine: string;
+		city: string;
+		code: number | string;
+		country: Country;
+	};
+	contact: [number, string];
+	isAvailable: boolean;
 }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,3 @@
-// type Alias
+//Type
 export type Price = 45 | 30 | 25;
 export type Country = 'Colombia' | 'Poland' | 'United Kingdom';
