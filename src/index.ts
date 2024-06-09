e918def51bd29cec34d0204775b3b1fce25903a0
Ethan <afrikapetersek@gmail.com>
2024-06-10 00:22:24 +0200

    Boolean Types mini-challenge

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,37 @@
-// Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to
-// all TypeScript weakness flags.
-// : number
-const reviewTotalDisplay = document.querySelector('#reviews');
+// Boolean Types mini-challenge
+// if the last reviewer is a loyalty User, can you add a star to the end of their name?
+// please do so in the existing function, and make sure to declare what type of 
+// parameters the function takes.
+// : boolean
+
+const reviewTotalDisplay = document.querySelector('#reviews')
 
 const reviews = [
-	{
-		name: 'Sheia',
-		stars: 5,
-		loyaltyUser: true,
-		date: '01-04-2021',
-	},
-	{
-		name: 'Andrzej',
-		stars: 3,
-		loyaltyUser: false,
-		date: '28-03-2021',
-	},
-	{
-		name: 'Omar',
-		stars: 4,
-		loyaltyUser: true,
-		date: '27-03-2021',
-	},
-];
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
 
 // Solution
-function showReviewTotal(value: number) {
-	reviewTotalDisplay.innerHTML = 'review total ' + value.toString();
+function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean) {
+    const iconDisplay = isLoyalty ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
 }
 
-showReviewTotal(reviews.length);
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
